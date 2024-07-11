75f092ad309ece475e0e14406550928f94c97588
Justin Macharia Mwai <justinmacharia78@gmail>
2024-07-11 07:26:42 +0300

    recursive approach

--- __tests__/roman_numerals.tests.js
+++ __tests__/roman_numerals.tests.js
@@ -1,4 +1,4 @@
-import {convertToRomanNumeral} from './../src/roman_numerals.js';
+import { convertToRomanNumeral, recursiveConvertToRoman } from './../src/roman_numerals.js';
 
 describe('Number to Roman Numerals Conversion', () => {
 
@@ -25,5 +25,30 @@ describe('Number to Roman Numerals Conversion', () => {
         expect(convertToRomanNumeral("abcds")).toEqual("invalid input");
         expect(convertToRomanNumeral("8")).toEqual("VIII");
         expect(convertToRomanNumeral("3999")).toEqual("MMMCMXCIX");
+    });
+
+    test('should recursively return correct Roman numerals given a number', async () => {
+        expect(recursiveConvertToRoman(10)).toEqual("X");
+        expect(recursiveConvertToRoman(66)).toEqual("LXVI");
+        expect(recursiveConvertToRoman(90)).toEqual("XC");
+        expect(recursiveConvertToRoman(99)).toEqual("XCIX");
+        expect(recursiveConvertToRoman(1)).toEqual("I");
+        expect(recursiveConvertToRoman(2)).toEqual("II");
+        expect(recursiveConvertToRoman(3)).toEqual("III");
+        expect(recursiveConvertToRoman(4)).toEqual("IV");
+        expect(recursiveConvertToRoman(5)).toEqual("V");
+        expect(recursiveConvertToRoman(6)).toEqual("VI");
+        expect(recursiveConvertToRoman(7)).toEqual("VII");
+        expect(recursiveConvertToRoman(8)).toEqual("VIII");
+        expect(recursiveConvertToRoman(9)).toEqual("IX");
+        expect(recursiveConvertToRoman(50)).toEqual("L");
+        expect(recursiveConvertToRoman(100)).toEqual("C");
+        expect(recursiveConvertToRoman(500)).toEqual("D");
+        expect(recursiveConvertToRoman(1000)).toEqual("M");
+        expect(recursiveConvertToRoman(0)).toEqual("invalid input");
+        expect(recursiveConvertToRoman(4000)).toEqual("invalid input");
+        expect(recursiveConvertToRoman("abcds")).toEqual("invalid input");
+        expect(recursiveConvertToRoman("8")).toEqual("VIII");
+        expect(recursiveConvertToRoman("3999")).toEqual("MMMCMXCIX");
     });
 });
--- src/roman_numerals.js
+++ src/roman_numerals.js
@@ -3,12 +3,48 @@ export const convertToRomanNumeral = (num) => {
         return "invalid input";
     const digits = String(+num).split("");
     const key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
-            "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
-            "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-        
+        "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+        "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+
     let roman = "";
     let i = 3;
     while (i--)
         roman = (key[+digits.pop() + (i * 10)] || "") + roman;
     return Array(+digits.join("") + 1).join("M") + roman;
 }
+
+// recursive approach
+const romanMatrix = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+];
+
+export const recursiveConvertToRoman = (num) => {
+    if (isNaN(num) || num < 1 || num > 3999)
+        return "invalid input";
+
+    return recursiveConvertToRomanHelper(num);
+};
+
+const recursiveConvertToRomanHelper = (num) => {
+    if (num === 0) {
+        return '';
+    }
+    for (var i = 0; i < romanMatrix.length; i++) {
+        if (num >= romanMatrix[i][0]) {
+            return romanMatrix[i][1] + recursiveConvertToRomanHelper(num - romanMatrix[i][0]);
+        }
+    }
+};
+
